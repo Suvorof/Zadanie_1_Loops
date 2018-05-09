e37d0003f87f5bcf751a45de6d0a3143756e7172
Konstantin Suvorov <konsu@inbox.ru>
2018-05-09 18:08:30 +0300

    Добавьте файлы проекта.

---
+++ Zadanie_1_2_Loops/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1_2_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for(i=0; i<=10; i++)
+            {
+                if(i%2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zadanie_1_Loops/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1_Loops
+{
+    class Program
+    {
+        static void Main()
+        /*{
+            int i;
+            int sum = 0;
+
+        for(i=0; i<=10; i++)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine("Сумма чисел лежащих в интервале от 0 до 10 включительно, равна " + sum);
+            Console.ReadKey();
+        }*/
+
+        {
+            int i;
+            for (i=0; i<=10; i++)
+                {
+                    if(i % 2 != 0)
+                        {
+                            Console.WriteLine("Все нечётные числа, лежащие в интервале от 0 до 10 "+ i);
+                        }
+                }
+            Console.ReadKey();
+        }
+    }
+}
